ab8283b8843d4c0d1de7ce64e01008aebd651f16
Jennifer Do <jdo17@uw.edu>
2023-05-01 16:40:57 -0700

    Add scroll to top button

--- js/agency.js
+++ js/agency.js
@@ -47,7 +47,24 @@ const observer = new IntersectionObserver(entries => {
     });
   });
   
-  const fadeUpElements = document.querySelectorAll('.fade-up');
-  fadeUpElements.forEach(element => {
-    observer.observe(element);
-  });  
+const fadeUpElements = document.querySelectorAll('.fade-up');
+fadeUpElements.forEach(element => {
+  observer.observe(element);
+});
+  
+// Scroll to the top
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#scroll-to-top-btn').fadeIn();
+    } else {
+      $('#scroll-to-top-btn').fadeOut();
+    }
+  });
+  
+  $('#scroll-to-top-btn').click(function() {
+    $('html, body').animate({scrollTop : 0}, 800);
+    return false;
+  });
+});
+    
